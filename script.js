387070ff77421601eca8afbadd964be3b5967c8f
889 <a889.149885@gmail.com>
2018-12-26 17:43:48 +0500

    2

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 // JavaScript Document
+
+//function myFunc(){
+//	var myButtons = document.getElementsByClassName("m-div");
+//	.style.backgroundColor = "red";
+//}
+
+function myFunction(myClickedBtn){
+	var myButtons = document.getElementsByClassName("m-div");
+	for(var i = 0; i<myButtons.length; i++){
+//		alert(i);
+		myButtons[i].style.backgroundColor = "deeppink";
+	}
+	myButtons[myClickedBtn].style.backgroundColor = "red";
+}
+
+//function myD1(){
+//	var myButtons = document.getElementsByClassName("m-div");
+//	myButtons[0].style.backgroundColor = "red";
+//	myButtons[1].style.backgroundColor = "deeppink";
+//	myButtons[2].style.backgroundColor = "deeppink";
+//	myButtons[3].style.backgroundColor = "deeppink";
+//	myButtons[4].style.backgroundColor = "deeppink";
+//}
+//
+//function myD2(){
+//	var myButtons = document.getElementsByClassName("m-div");
+//	myButtons[1].style.backgroundColor = "red";
+//	myButtons[0].style.backgroundColor = "deeppink";
+//	myButtons[2].style.backgroundColor = "deeppink";
+//	myButtons[3].style.backgroundColor = "deeppink";
+//	myButtons[4].style.backgroundColor = "deeppink";
+//}
+//
+//function myD3(){
+//	var myButtons = document.getElementsByClassName("m-div");
+//	myButtons[2].style.backgroundColor = "red";
+//	myButtons[0].style.backgroundColor = "deeppink";
+//	myButtons[1].style.backgroundColor = "deeppink";
+//	myButtons[3].style.backgroundColor = "deeppink";
+//	myButtons[4].style.backgroundColor = "deeppink";
+//}
+//
+//function myD4(){
+//	var myButtons = document.getElementsByClassName("m-div");
+//	myButtons[3].style.backgroundColor = "red";
+//	myButtons[1].style.backgroundColor = "deeppink";
+//	myButtons[2].style.backgroundColor = "deeppink";
+//	myButtons[0].style.backgroundColor = "deeppink";
+//	myButtons[4].style.backgroundColor = "deeppink";
+//}
+//
+//function myD5(){
+//	var myButtons = document.getElementsByClassName("m-div");
+//	myButtons[4].style.backgroundColor = "red";
+//	myButtons[1].style.backgroundColor = "deeppink";
+//	myButtons[2].style.backgroundColor = "deeppink";
+//	myButtons[3].style.backgroundColor = "deeppink";
+//	myButtons[0].style.backgroundColor = "deeppink";
+//}
